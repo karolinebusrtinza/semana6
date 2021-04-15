a9719d7a38365060076b20cf59d56bd73b672938
karolinebusrtinza <bustinzakaroline@gmail.com>
2021-04-15 10:48:05 -0500

    initial commit

---
+++ Semana6colecciondejuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Semana6colecciondejuegos
+//
+//  Created by mbtec22 on 4/15/21.
+//  Copyright © 2021 Tecsup.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
